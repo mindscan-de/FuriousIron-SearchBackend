e79c903fbdfca8b795ca580c770dbea9775e98e9
mindscan-de <maxim.gansert@gmail.com>
2019-05-11 22:15:25 +0200

    add (empty) backend service for cached content

---
+++ src/main/java/de/mindscan/furiousiron/searchbackend/CachedContentRESTfulService.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2019 Maxim Gansert, Mindscan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package de.mindscan.furiousiron.searchbackend;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+/**
+ * 
+ */
+@javax.ws.rs.Path( "/cached" )
+public class CachedContentRESTfulService {
+
+    @javax.ws.rs.Path( "/content" )
+    @GET
+    @Produces( "text/plain" )
+    public String getCachedResult( @QueryParam( "p" ) String path ) {
+
+        System.out.println( "We should provide content of:" + path );
+
+        return "Content could not be retrieved";
+    }
+
+}
