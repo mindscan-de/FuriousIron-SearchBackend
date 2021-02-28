3f85dd1e4eec733b5c3f603454051f43db257c48
mindscan-de <maxim.gansert@gmail.com>
2021-02-28 19:36:21 +0100

    implement first demo for the wordlist penalty compiler.

--- src/main/java/de/mindscan/furiousiron/search2/WordlistTrigramPenaltyCompiler.java
+++ src/main/java/de/mindscan/furiousiron/search2/WordlistTrigramPenaltyCompiler.java
@@ -25,19 +25,128 @@
  */
 package de.mindscan.furiousiron.search2;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import de.mindscan.furiousiron.index.trigram.TrigramOccurence;
+import de.mindscan.furiousiron.indexer.SimpleWordUtils;
 
 /**
  * 
  */
 public class WordlistTrigramPenaltyCompiler {
+    public static class WordScore {
+        private String word;
+        private int score;
+
+        public WordScore( String word ) {
+            this( word, 1 );
+        }
+
+        public WordScore( String word, int score ) {
+            this.word = word;
+            this.score = score;
+        }
+
+        /**
+         * @param i
+         */
+        public void increase( int i ) {
+            this.score += 1;
+        }
+
+        /**
+         * @param i
+         */
+        public void decrease( int i ) {
+            this.score -= i;
+        }
+
+        /**
+         * @return the score
+         */
+        public int getScore() {
+            return score;
+        }
+
+        /** 
+         * {@inheritDoc}
+         */
+        @Override
+        public String toString() {
+            return "'" + word + "': " + score;
+        }
+    }
 
     public Collection<String> getOrderedWordlist( Collection<TrigramOccurence> allTrigramOccurrences, Collection<String> wordlist,
                     Collection<TrigramUsage> usage ) {
 
-        return wordlist;
+        // no word list -> nothing to do here
+        if (wordlist.isEmpty()) {
+            return wordlist;
+        }
+
+        // create a map of the wordlist, and init with each word with a defaultscore
+        Map<String, WordScore> wordMapScore = new HashMap<>();
+        wordlist.stream().map( word -> {
+            System.out.println( "adding to map: " + word );
+            initWordScoce( wordMapScore, word );
+            return true;
+        } ).collect( Collectors.toList() );
+
+        System.out.println( "WordMapScore Map Size = " + wordMapScore.size() );
+
+        // create a map of usages
+        Map<String, TrigramUsage> usageMap = new HashMap<>();
+        usage.stream().map( ( tusage ) -> usageMap.put( tusage.getTrigram(), tusage ) ).collect( Collectors.toList() );
+
+        for (String word : wordlist) {
+            Collection<String> trigramsForWord = SimpleWordUtils.getUniqueTrigramsFromWord( word );
+            WordScore wordScore = wordMapScore.get( word );
+            for (String trigram : trigramsForWord) {
+                if (usageMap.containsKey( trigram )) {
+                    // it was processed
+                    TrigramUsage trigramUsage = usageMap.get( trigram );
+                    if (trigramUsage.isSuccess()) {
+                        wordScore.increase( 1 );
+                    }
+                    else {
+                        wordScore.decrease( 1 );
+                    }
+                }
+                else {
+                    // it is unknown 
+                    // penalize
+                }
+            }
+        }
+
+        List<String> wordlistToSort = new ArrayList<>( wordlist );
+        wordlistToSort.sort( Comparator.comparingInt( word -> extracted( (String) word, wordMapScore ) ).reversed() );
+
+        for (String word : wordlistToSort) {
+            try {
+                System.out.println( wordMapScore.get( word ).toString() );
+            }
+            catch (Exception e) {
+                System.out.println( "unknown word: " + word );
+            }
+        }
+
+        return wordlistToSort;
+    }
+
+    private void initWordScoce( Map<String, WordScore> wordMapScore, String word ) {
+        wordMapScore.put( word, new WordScore( word ) );
+    }
+
+    private int extracted( String word, Map<String, WordScore> wordMapScore ) {
+        return wordMapScore.get( word ).getScore();
     }
 
 }
--- src/test/java/de/mindscan/furiousiron/search2/WordlistTrigramPenaltyCompilerTest.java
+++ src/test/java/de/mindscan/furiousiron/search2/WordlistTrigramPenaltyCompilerTest.java
@@ -1,5 +1,7 @@
 package de.mindscan.furiousiron.search2;
 
+import static de.mindscan.furiousiron.search2.TrigramUsage.TrigramUsageState.FAILED;
+import static de.mindscan.furiousiron.search2.TrigramUsage.TrigramUsageState.SUCCESS;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
@@ -39,7 +41,7 @@ public class WordlistTrigramPenaltyCompilerTest {
 
         Collection<TrigramOccurence> allTrigramOccurrences = createTrigramOccurrenceList();
         Collection<String> wordlist = createWordlist();
-        Collection<TrigramUsage> usage = Collections.emptyList();
+        Collection<TrigramUsage> usage = createFullTrigramUsageList();
 
         // act
         Collection<String> result = trigramPenaltyCompiler.getOrderedWordlist( allTrigramOccurrences, wordlist, usage );
@@ -92,4 +94,42 @@ public class WordlistTrigramPenaltyCompilerTest {
         return Arrays.asList( "import", "package", "class", "index", "store", "abstract", "field", "hashmap", "string" );
     }
 
+    private List<TrigramUsage> createFullTrigramUsageList() {
+        return Arrays.asList( //
+                        new TrigramUsage( "shm", SUCCESS ), //
+                        new TrigramUsage( "hma", SUCCESS ), //
+                        new TrigramUsage( "abs", SUCCESS ), //
+                        new TrigramUsage( "bst", SUCCESS ), // 
+                        new TrigramUsage( "ash", FAILED ), //
+                        new TrigramUsage( "sto", SUCCESS ), //
+                        new TrigramUsage( "map", FAILED ), // 
+                        new TrigramUsage( "rac", SUCCESS ), //
+                        new TrigramUsage( "iel", SUCCESS ), //
+                        new TrigramUsage( "eld", FAILED ), // 
+                        new TrigramUsage( "has", FAILED ), // 
+                        new TrigramUsage( "fie", SUCCESS ), //
+                        new TrigramUsage( "tra", SUCCESS ), // 
+                        new TrigramUsage( "dex", SUCCESS ), //
+                        new TrigramUsage( "act", FAILED ), //
+                        new TrigramUsage( "ind", SUCCESS ), // 
+                        new TrigramUsage( "rin", FAILED ), //
+                        new TrigramUsage( "ore", FAILED ), // 
+                        new TrigramUsage( "tor", FAILED ), //
+                        new TrigramUsage( "nde", FAILED ), //
+                        new TrigramUsage( "cla", FAILED ), //
+                        new TrigramUsage( "mpo", SUCCESS ), //
+                        new TrigramUsage( "ass", FAILED ), //
+                        new TrigramUsage( "str", FAILED ), //
+                        new TrigramUsage( "por", FAILED ), //
+                        new TrigramUsage( "kag", FAILED ), //
+                        new TrigramUsage( "cka", FAILED ), //
+                        new TrigramUsage( "ort", FAILED ), //
+                        new TrigramUsage( "tri", FAILED ), // 
+                        new TrigramUsage( "ing", FAILED ), //
+                        new TrigramUsage( "ack", FAILED ), // 
+                        new TrigramUsage( "las", FAILED ), // 
+                        new TrigramUsage( "pac", FAILED ), //
+                        new TrigramUsage( "imp", FAILED ), //
+                        new TrigramUsage( "age", FAILED ) );
+    }
 }
