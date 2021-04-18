6fd36527988545a8c29a22bb548b72fe31b1066f
mindscan-de <maxim.gansert@gmail.com>
2021-04-18 13:52:27 +0200

    trying a bit on the proof of concept for hash free bloom filters.

--- src/main/java/de/mindscan/furiousiron/incubator/hfb/HashFreeBloom.java
+++ src/main/java/de/mindscan/furiousiron/incubator/hfb/HashFreeBloom.java
@@ -28,6 +28,7 @@ package de.mindscan.furiousiron.incubator.hfb;
 import java.math.BigInteger;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -38,13 +39,36 @@ import de.mindscan.furiousiron.search.Search;
  * Actually the idea is to implement an effective hash-free alternative to 
  * bloom-filters. Bloom filters are nice, but somehow over-rated, and the 
  * most annoying thing is the requirement for a hash function, which actually
- * compresses the input. I don't want to spend cpu-cycles on hashing things 
- * over and over again. We often enough have hashes already in place, so
- * hashing the hashes is just a big waste of compute.
+ * compresses the input. A compression function where the non set items tend 
+ * to collide and the indexed items tend to collide would be much appreciated.
+ * But the later is  not going to happen.
+ * 
+ * I don't want to spend cpu-cycles on hashing things over and over again. We 
+ * often enough have hashes already in place, so hashing the hashes is just a 
+ * waste of compute.
  * 
  * I also want a lookup of O(1) for a no/maybe decision. What's also useful is,
  * that different filters of the same size and the same position can be joined
  * before applying two filter operations.
+ * 
+ * Why are there no extra hashes needed?
+ * 
+ * The trick is simple:
+ * 
+ * The documentid is already a hash(of the document_path) / I use an md5 atm,
+ * but can be of course a sha256. So using a slice of bits of the document id 
+ * basically is a hash-calculation taking the bits from another part of the 
+ * documentid is a completely different hash function. So filtering the 
+ * documentids is reduced to simple bit arithmetic and lookups in a bit flag
+ * array. So the hashfunction is reduced to two parameters, slice-position and
+ * slice mask. and the resulting hashfunction is to shift right and then to
+ * apply an and operation -> (BAM!!) hashvalue calculated.
+ * 
+ *  This hash function will calculate to the same hash for the same input.
+ * 
+ * These internal bitflag arrays can be stored using golomb coding, so then 
+ * even very sparse bit flag arrays can be stored memory efficient. (In case 
+ * you need a high dropout rate) 
  */
 public class HashFreeBloom {
 
@@ -57,8 +81,9 @@ public class HashFreeBloom {
         index = new Search( Paths.get( "D:\\Analysis\\CrawlerProjects", "Indexed" ) );
     }
 
-    public void createHFBFilter() {
-        String[] x = new String[] { "imp" };
+    public HFBFilterBank createHFBFilter() {
+        // RDI 7883
+        String[] x = new String[] { "rdi" };
 
         for (int i = 0; i < x[0].length(); i++) {
             System.out.println( Integer.toString( x[0].charAt( i ), 16 ) );
@@ -80,5 +105,37 @@ public class HashFreeBloom {
 
         // ein filter dieser Art hat eine rejection rate von 81 prozent, wenn bspw der Faktor 5 verwendet wird, aber in wirklichkeit irgendwo zwischen 3 und 6
         // eine kombination aus zwei filtern gleicher größe und gleicher hashlänge können vorher kombiniert werden und können die rejection rate erhöhen.
+
+        return bank;
     }
+
+    public void filter( HFBFilterBank filterbank ) {
+        // RDI 7883
+        // RDT 335        
+        // RDI && RDT -> 212
+        String[] x = new String[] { "rdt" };
+
+        for (int i = 0; i < x[0].length(); i++) {
+            System.out.println( Integer.toString( x[0].charAt( i ), 16 ) );
+        }
+
+        List<TrigramOccurrence> trigramOccurrencesSortedByOccurrence = index.getTrigramOccurrencesSortedByOccurrence( Arrays.asList( x ) );
+        Set<String> y = index.collectDocumentIdsForTrigramsOpt( Arrays.asList( x ) );
+
+        TrigramOccurrence trigramOccurrence = trigramOccurrencesSortedByOccurrence.get( 0 );
+        System.out.println( trigramOccurrence );
+
+        Set<String> newSet = new HashSet<>();
+        for (String documentId : y) {
+            BigInteger bi = new BigInteger( documentId, 16 );
+
+            if (filterbank.containsDocumentId( bi )) {
+                newSet.add( documentId );
+            }
+        }
+
+        System.out.println( "Newset.size...." );
+        System.out.println( newSet.size() );
+    }
+
 }
--- src/test/java/de/mindscan/furiousiron/incubator/hfb/HashFreeBloomTest.java
+++ src/test/java/de/mindscan/furiousiron/incubator/hfb/HashFreeBloomTest.java
@@ -2,8 +2,6 @@ package de.mindscan.furiousiron.incubator.hfb;
 
 import org.junit.jupiter.api.Test;
 
-import de.mindscan.furiousiron.incubator.hfb.HashFreeBloom;
-
 public class HashFreeBloomTest {
 
     @Test
@@ -13,6 +11,30 @@ public class HashFreeBloomTest {
 
         // act
         hashFreeBloom.createHFBFilter();
+
+        // assert
+        // cpxuas
+    }
+
+    @Test
+    public void testFilter() throws Exception {
+        // arrange
+        HashFreeBloom hashFreeBloom = new HashFreeBloom();
+        HFBFilterBank filterbank = hashFreeBloom.createHFBFilter();
+
+        // act
+        hashFreeBloom.filter( filterbank );
+
+        // +RDI +RDT -> 212
+        // RDT -> 337
+        // Filter RDT with RDI 1 Filter -> 244  / elimination rate   93/125 
+        // Filter RDT with RDI 2 Filters -> 221 / elimination rate   23/125 | 116/125
+        // Filter RDT with RDI 3 Filters -> 213 / elimination rate    8/125 | 124/125 
+        // Filter RDT with RDI 4 Filters -> 213 
+        // Filter RDT with RDI 5 Filters -> 213
+        // Filter RDT with RDI 6 Filters -> 212 / elimination rate    1/125 | 125/125
+        // Filter RDT with RDI 7 Filters -> 212
+        // Filter RDT with RDI 8 Filters -> 212
 
         // assert
         // cpxuas
