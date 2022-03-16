9ed527027063bb754ca206e7ea6c679bfdbab8fc
mindscan-de <maxim.gansert@gmail.com>
2022-03-16 18:47:09 +0100

    test for particular tokens in list using the equals contract of SearchQieryTokenImpl.

--- src/test/java/de/mindscan/furiousiron/search/query/tokenizer/SearchQueryTokenizerImplTest.java
+++ src/test/java/de/mindscan/furiousiron/search/query/tokenizer/SearchQueryTokenizerImplTest.java
@@ -3,12 +3,16 @@ package de.mindscan.furiousiron.search.query.tokenizer;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 
 import java.util.List;
 
 import org.junit.Test;
 
 import de.mindscan.furiousiron.search.query.token.SearchQueryToken;
+import de.mindscan.furiousiron.search.query.token.SearchQueryTokenImpl;
+import de.mindscan.furiousiron.search.query.token.SearchQueryTokenType;
+import de.mindscan.furiousiron.search.query.token.SearchQueryTokens;
 
 public class SearchQueryTokenizerImplTest {
 
@@ -37,6 +41,18 @@ public class SearchQueryTokenizerImplTest {
     }
 
     @Test
+    public void testParse_PlusTestQueryString_containsPlusTokenAndTextTokenInList() throws Exception {
+        // arrange
+        SearchQueryTokenizerImpl tokenizer = new SearchQueryTokenizerImpl();
+
+        // act
+        List<SearchQueryToken> result = tokenizer.parse( "+test" );
+
+        // assert
+        assertThat( result, contains( SearchQueryTokens.OPERATOR_PLUS, textToken( "test" ) ) );
+    }
+
+    @Test
     public void testParse_MinusTestQueryString_TwoTokensInTokenList() throws Exception {
         // arrange
         SearchQueryTokenizerImpl tokenizer = new SearchQueryTokenizerImpl();
@@ -46,6 +62,22 @@ public class SearchQueryTokenizerImplTest {
 
         // assert
         assertThat( result, hasSize( 2 ) );
+    }
+
+    @Test
+    public void testParse_MinusTestQueryString_containsMinusTokenAndTextTokenInList() throws Exception {
+        // arrange
+        SearchQueryTokenizerImpl tokenizer = new SearchQueryTokenizerImpl();
+
+        // act
+        List<SearchQueryToken> result = tokenizer.parse( "-test" );
+
+        // assert
+        assertThat( result, contains( SearchQueryTokens.OPERATOR_MINUS, textToken( "test" ) ) );
+    }
+
+    private SearchQueryTokenImpl textToken( String value ) {
+        return new SearchQueryTokenImpl( SearchQueryTokenType.SEARCHTERM, value );
     }
 
 }
