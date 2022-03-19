a7ce52575d6914c248c4729f03df36120ecd9870
mindscan-de <maxim.gansert@gmail.com>
2022-03-19 11:24:50 +0100

    add unit tests for testing metadata query tokenization.

--- src/test/java/de/mindscan/furiousiron/search/query/tokenizer/SearchQueryTokenizerImplTest.java
+++ src/test/java/de/mindscan/furiousiron/search/query/tokenizer/SearchQueryTokenizerImplTest.java
@@ -112,6 +112,31 @@ public class SearchQueryTokenizerImplTest {
         assertThat( result, contains( SearchQueryTokens.OPERATOR_PLUS, textToken( "test" ), SearchQueryTokens.OPERATOR_MINUS, exactTextToken( "test" ) ) );
     }
 
+    @Test
+    public void testParse_Plus1234Minus123456QueryString_containsPLusToken1234AndMinus123456Token() throws Exception {
+        // arrange
+        SearchQueryTokenizerImpl tokenizer = new SearchQueryTokenizerImpl();
+
+        // act
+        List<SearchQueryToken> result = tokenizer.parse( "+1234 -123456" );
+
+        // assert
+        assertThat( result, contains( SearchQueryTokens.OPERATOR_PLUS, textToken( "1234" ), SearchQueryTokens.OPERATOR_MINUS, textToken( "123456" ) ) );
+    }
+
+    @Test
+    public void testParse_PlusTestNotPythonFile_containsPlusTokenTestAndMinusFiletypeTyphon() throws Exception {
+        // arrange
+        SearchQueryTokenizerImpl tokenizer = new SearchQueryTokenizerImpl();
+
+        // act
+        List<SearchQueryToken> result = tokenizer.parse( "+test -filetype:python" );
+
+        // assert
+        assertThat( result, contains( SearchQueryTokens.OPERATOR_PLUS, textToken( "test" ), SearchQueryTokens.OPERATOR_MINUS, textToken( "filetype" ),
+                        SearchQueryTokens.OPERATOR_DOUBLECOLON, textToken( "python" ) ) );
+    }
+
     private SearchQueryTokenImpl textToken( String value ) {
         return new SearchQueryTokenImpl( SearchQueryTokenType.SEARCHTERM, value );
     }
