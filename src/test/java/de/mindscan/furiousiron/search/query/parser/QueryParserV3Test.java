24ab8e2381701152289de992f5580b99adb121f8
mindscan-de <maxim.gansert@gmail.com>
2022-03-21 22:18:52 +0100

    add two unit tests to align QueryParserV3 with the original QueryParser

--- src/test/java/de/mindscan/furiousiron/search/query/parser/QueryParserV3Test.java
+++ src/test/java/de/mindscan/furiousiron/search/query/parser/QueryParserV3Test.java
@@ -1,10 +1,39 @@
 package de.mindscan.furiousiron.search.query.parser;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+
 import org.junit.jupiter.api.Test;
 
+import de.mindscan.furiousiron.query.ast.EmptyNode;
 import de.mindscan.furiousiron.query.ast.QueryNode;
 
 public class QueryParserV3Test {
+
+    @Test
+    public void testParseQuery_emptyString_returnsEmptyNode() {
+        // Arrange
+        QueryParserV3 parserV3 = new QueryParserV3();
+
+        // Act
+        QueryNode result = parserV3.parseQuery( "" );
+
+        // Assert
+        assertThat( result, is( instanceOf( EmptyNode.class ) ) );
+    }
+
+    @Test
+    public void testParseQuery_nullAsString_returnsEmptyNode() {
+        // Arrange
+        QueryParserV3 parserV3 = new QueryParserV3();
+
+        // Act
+        QueryNode result = parserV3.parseQuery( null );
+
+        // Assert
+        assertThat( result, is( instanceOf( EmptyNode.class ) ) );
+    }
 
     @Test
     public void testParseQuery_testTextQuery_() throws Exception {
@@ -15,7 +44,7 @@ public class QueryParserV3Test {
         QueryNode result = parserV3.parseQuery( "test" );
 
         // assert
-        // cpxuas
+        // assertThat( result, is( instanceOf( TextNode.class ) ) );
 
     }
 
