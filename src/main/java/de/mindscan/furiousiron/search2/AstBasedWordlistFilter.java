b5573a5e9d60512fdccf758e4db1d45359f6d3b2
mindscan-de <maxim.gansert@gmail.com>
2022-04-17 23:27:47 +0200

    there should be an impl capable of filtering word lists and metadata

--- src/main/java/de/mindscan/furiousiron/search2/AstBasedWordlistFilter.java
+++ src/main/java/de/mindscan/furiousiron/search2/AstBasedWordlistFilter.java
@@ -53,7 +53,7 @@ public class AstBasedWordlistFilter {
      * @return <code>true</code> iff the AST matches a given word list.
      */
     static boolean isAstMatchingToWordlist( QueryNode ast, List<String> documentWordlist ) {
-
+        // TODO maybe add the metadata wordlist here as well.
         if (ast instanceof TextNode) {
             String wordToSearch = ast.getContent();
 
@@ -135,7 +135,9 @@ public class AstBasedWordlistFilter {
         }
 
         // TODO: implement Wordlist / metawordlist matching...
+        // problem is that we can not handle the metadata wordlist correctly here....
         if (ast instanceof MetaDataTextNode) {
+            // depending of whether we are in an excluide node we should basically return false and oherwise true
             // just do the right thing at this moment - should be improved soon.
             return true;
         }
