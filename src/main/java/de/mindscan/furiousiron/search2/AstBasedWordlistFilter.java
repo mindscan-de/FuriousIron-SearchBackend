157687d91991cd3a3ec8ddc06f78b0ed0d069c35
mindscan-de <maxim.gansert@gmail.com>
2022-04-18 19:26:28 +0200

    prepare metadata search by providing another ast based wordfilter.

--- src/main/java/de/mindscan/furiousiron/search2/AstBasedWordlistFilter.java
+++ src/main/java/de/mindscan/furiousiron/search2/AstBasedWordlistFilter.java
@@ -27,6 +27,7 @@ package de.mindscan.furiousiron.search2;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import de.mindscan.furiousiron.query.ast.AndNode;
 import de.mindscan.furiousiron.query.ast.ExactMatchingTextNode;
@@ -140,6 +141,118 @@ public class AstBasedWordlistFilter {
             // depending of whether we are in an excluide node we should basically return false and oherwise true
             // just do the right thing at this moment - should be improved soon.
             return true;
+        }
+
+        throw new RuntimeException( "This Node type is not supported: " + String.valueOf( ast ) );
+    }
+
+    /**
+     * This method tests whether the given AST of QueryNodes matches a wordlist (e.g. a wordlist of an indexed document)
+     * This is a word-based filtering mechanism, which is slightly more expensive in terms of compute costs after the 
+     * trigram filters are exhausted. This kind of search like testing whether a search term is part of a word, will
+     * result in quadratic or higher computing costs (well, if you don't optimize). 
+     *  
+     * @param ast the ast which is applied onto a wordlist
+     * @param documentWordlist a word list of a document
+     * @return <code>true</code> iff the AST matches a given word list.
+     */
+    static boolean isAstMatchingToWordlistAndMetadata( QueryNode ast, List<String> documentWordlist, Map<String, String> metadata ) {
+        // TODO maybe add the metadata wordlist here as well.
+        if (ast instanceof TextNode) {
+            String wordToSearch = ast.getContent();
+
+            if (documentWordlist.contains( wordToSearch )) {
+                return true;
+            }
+
+            int wordToSearchLength = wordToSearch.length();
+
+            for (String documentWord : documentWordlist) {
+                if (documentWord.length() > wordToSearchLength) {
+                    if (documentWord.contains( wordToSearch )) {
+                        return true;
+                    }
+                }
+            }
+
+            // it is neither contained fully nor partially. 
+            return false;
+        }
+
+        if (ast instanceof ExactMatchingTextNode) {
+            // TODO: implement a correct strategy to handle exact matching text nodes
+            // TODO: we must distinguish between a phrase containing WS and such, then this is more complicated.
+
+            return documentWordlist.contains( ast.getContent() );
+        }
+
+        if (ast instanceof AndNode) {
+            if (ast.hasChildren()) {
+                Collection<QueryNode> children = ast.getChildren();
+                for (QueryNode queryNode : children) {
+                    // early exit in case of a "false" - no need to check further if word is not found.
+                    if (!isAstMatchingToWordlistAndMetadata( queryNode, documentWordlist, metadata )) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            else {
+                return true;
+            }
+        }
+
+        if (ast instanceof OrNode) {
+            if (ast.hasChildren()) {
+                Collection<QueryNode> children = ast.getChildren();
+                for (QueryNode queryNode : children) {
+                    // early exit in case of a "true" - no need to check further if other word is also found.
+                    if (isAstMatchingToWordlistAndMetadata( queryNode, documentWordlist, metadata )) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            else {
+                return false;
+            }
+        }
+
+        if (ast instanceof IncludingNode) {
+            if (ast.hasChildren()) {
+                QueryNode first = ast.getChildren().iterator().next();
+                return isAstMatchingToWordlistAndMetadata( first, documentWordlist, metadata );
+            }
+            else {
+                return true;
+            }
+        }
+
+        if (ast instanceof ExcludingNode) {
+            if (ast.hasChildren()) {
+                QueryNode first = ast.getChildren().iterator().next();
+                return !isAstMatchingToWordlistAndMetadata( first, documentWordlist, metadata );
+            }
+            else {
+                return false;
+            }
+        }
+
+        if (ast instanceof MetaDataTextNode) {
+            String key = ((MetaDataTextNode) ast).getKey();
+            String value = ast.getContent();
+
+            if (!metadata.containsKey( key )) {
+                return false;
+            }
+
+            String metadataContent = metadata.get( key );
+
+            if (metadataContent == null) {
+                return false;
+            }
+
+            return metadataContent.contains( value );
         }
 
         throw new RuntimeException( "This Node type is not supported: " + String.valueOf( ast ) );
